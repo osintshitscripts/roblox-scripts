c311e6640503f3f4aa184fd7df55a49c6e865337
osintshitscripts <osintshit@gmail.com>
2026-03-21 13:30:18 -0400

    Update bladeballlscriptloadergui.lua

--- bladeballlscriptloadergui.lua
+++ bladeballlscriptloadergui.lua
@@ -1,1 +1,15 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/osintshitscripts/roblox-scripts/refs/heads/main/bladeballgui.lua"))()
+
+local link = "https://discord.gg/xYbFazNtDc"
+
+-- copy to clipboard
+if setclipboard then
+    setclipboard(link)
+end
+
+-- notification
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Join the Discord",
+    Text = "Invite copied to clipboard!",
+    Duration = 5
+})
